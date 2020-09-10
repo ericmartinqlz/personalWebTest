2fe5111df7271a07966ce2121ff461ce375e7f2e
Eric Martin <65438619+ericmartinqlz@users.noreply.github.com>
2020-09-11 00:17:36 +0700

    Update works.js
    Tambah data Work Repo Github

--- assets/scripts/data/works.js
+++ assets/scripts/data/works.js
@@ -5,6 +5,13 @@ const works = [
         ghURL : 'https://github.com/ericmartinqlz/mov-info',
         descriptive : `This is a project that aims to complete a Dicoding class submission. 
         This project serves to search for movie data using the API from themoviedb.org`
+    },
+    {
+        repoName : 'personalWebTest',
+        repoURL : '',
+        ghURL: 'https://github.com/ericmartinqlz/personalWebTest',
+        descriptive : `Latihan membuat personal web (Only Testing). 
+        Still learning CSS, JS, and WebComponent.`
     }
 ]
 
